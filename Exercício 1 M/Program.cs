11113cbad1a3691ac5cc1c9377d764b80aea42bd
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-13 18:54:57 -0300

    Add files via upload

---
+++ Exercício 1 M/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Exercício_1_M
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] a;
+
+            string[] vetor = Console.ReadLine().Split(' ');
+            int m = int.Parse(vetor[0]);
+            int n = int.Parse(vetor[1]);
+
+            a = new int[m, n];
+
+           
+
+            for (int l = 0; l < m; l++)
+            {
+                string[] vetor1 = Console.ReadLine().Split(' ');
+                for (int c = 0; c < n; c++)
+                {
+                    a[l, c] = int.Parse(vetor1[c]);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Valores Negativos : ");
+            for (int l = 0; l < m; l++)
+            {
+
+                for (int c = 0; c < n; c++)
+                {
+                    if (a[l,c] < 0)
+                    {
+                        Console.WriteLine(a[l, c]);
+                    }
+                  
+                }
+
+               
+               
+
+            }
+        }
+    }
+}
